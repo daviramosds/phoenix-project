53e7a9f7cece90a1e01bac3503eff002542d6b1d
daviramosds <davirds.dev@gmail.com>
2023-03-01 16:47:13 -0300

    add: exec function

--- lib/commands.py
+++ lib/commands.py
@@ -4,8 +4,11 @@ from urllib import parse
 def test():
     print('test command')
 
-
 def google_search(text):  # say google search {search}
     web_encode = parse.quote(text)
     webbrowser.open(f"https://www.google.com/search?q={web_encode}")
     print(f'google searching {text}')
+
+def exec(cmd):
+    print(cmd)
+    return 'output'
