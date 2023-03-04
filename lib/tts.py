dbeffd64fb4cfe542e372f491a83fb770c6f102f
daviramosds <davirds.dev@gmail.com>
2023-03-04 09:32:30 -0300

    update: renaming vars

--- lib/tts.py
+++ lib/tts.py
@@ -5,9 +5,9 @@ from os import getenv
 def generate_speech(text):
     tts = gTTS(text, lang="en")
     file_id = randint(1000, 9999)
-    AF_PATH = getenv('TMP_FILE_PATH')
-    AF_PREFIX = getenv('AUDIO_FILE_PREFIX')
+    TMP_FILE_PATH = getenv('TMP_FILE_PATH')
+    AUDIO_FILE_PREFIX = getenv('AUDIO_FILE_PREFIX')
     file_name = f'{file_id}.mp3'
-    file_path = f'{AF_PATH}{AF_PREFIX}{file_name}'
+    file_path = f'{TMP_FILE_PATH}{AUDIO_FILE_PREFIX}{file_name}'
     tts.save(file_path)
     return file_name
