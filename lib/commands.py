ebe9280af2f47ad8d948d3f7cdb40f1714a1310e
daviramosds <davirds.dev@gmail.com>
2023-03-05 08:08:29 -0300

    add: respond function

--- lib/commands.py
+++ lib/commands.py
@@ -3,50 +3,42 @@ from urllib import parse
 from flask import jsonify
 from lib import tts
 
-
 def test():
     print('test command')
-
 
 def google_search(text):  # say google search {search}
     web_encode = parse.quote(text)
     webbrowser.open(f'https://www.google.com/search?q={web_encode}')
 
-
 def show_my_devices():
     webbrowser.open('https://www.google.com/android/find/')
 
+def respond(text):
+    audio_b64 = tts.generate_speech(text)
+    return jsonify({'message': 'invalid command', 'audio_b64': audio_b64})
 
 def exec(cmd):
     if 'google search' in cmd:
         cmd = cmd.split('google search')
+        print(cmd)
         term = cmd[1].strip()
         if 'about' in term.split(' ')[0]:
             term = term.replace('about', '')
-        message = f'google searching {term}'
         google_search(term)
-        audio_b64 = tts.generate_speech(message)
-        return jsonify({'message': message, 'audio_b64': audio_b64})
+        return respond(f'google searching {term}')
 
     if 'show my devices' in cmd:
-        cmd = 'find my devices'
-        message = f'showing your devices'
         show_my_devices()
-        audio_b64 = tts.generate_speech(message)
-        return jsonify({'message': message, 'audio_b64': audio_b64})
+        return respond('showing your devices')
 
     if 'test' in cmd:
-        print('TEST COMMAND')
-        return jsonify({'message': 'test command'})
+        return respond('TEST COMMAND')
 
     if 'generate speech' in cmd:
-        audio_b64 = tts.generate_speech('Hello master! what can i do for you?')
-        return jsonify({'audio_b64': audio_b64})
+        return respond('Hello master! what can i do for you?')
 
-    if 'thanks' in cmd or 'thank you':
-        audio_b64 = tts.generate_speech("You're Welcome")
-        return jsonify({'audio_b64': audio_b64})
+    if 'thanks' in cmd or 'thank you' in cmd:
+        return respond("You're Welcome")
 
     else:
-        audio_b64 = tts.generate_speech('invalid command')
-        return jsonify({'message': 'invalid command', 'audio_b64': audio_b64})
+        return respond('invalid command')
